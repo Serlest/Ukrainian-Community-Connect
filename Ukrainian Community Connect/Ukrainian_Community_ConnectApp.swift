c1a6a10e92c2b1e08441dced08c40c15b8130e39
Serlest <timofeev.ph@outlook.com>
2024-01-23 16:48:08 +0100

    Initial Commit

---
+++ Ukrainian Community Connect/Ukrainian_Community_ConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ukrainian_Community_ConnectApp.swift
+//  Ukrainian Community Connect
+//
+//  Created by Philipp Timofeev on 23.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Ukrainian_Community_ConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
